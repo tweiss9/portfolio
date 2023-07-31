aa1ca9107cc19be522733600fa9f16eef5621897
tweiss9 <tylerhweiss@gmail.com>
2023-07-30 23:59:24 -0400

    reCAPTCHA on form *no env yet

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,27 @@
+let recaptchaResponse = '';
+
+function onRecaptchaCompleted(response) {
+  recaptchaResponse = response;
+  fetch("/verify-recaptcha", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ recaptchaResponse }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        document.getElementById("myForm").submit();
+      } else {
+        console.error("reCAPTCHA validation failed");
+      }
+    })
+    .catch((error) => {
+      console.error("Error verifying reCAPTCHA:", error);
+    });
+}
+
 function scrollToSection(sectionId) {
   const targetSection = document.querySelector(sectionId);
   targetSection.scrollIntoView({ behavior: "smooth" });
@@ -11,35 +35,55 @@ function handleSubmit(event) {
   const messageInput = document.getElementById("message");
 
   let isValid = true;
-  isValid = toggleValidation(nameInput, !nameInput.value.trim(), 'nameError') && isValid;
-  isValid = toggleValidation(emailInput, !emailInput.value.trim() || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value.trim()), 'emailError') && isValid;
-  isValid = toggleValidation(messageInput, !messageInput.value.trim(), 'messageError') && isValid;
+  isValid =
+    toggleValidation(nameInput, !nameInput.value.trim(), "nameError") &&
+    isValid;
+  isValid =
+    toggleValidation(
+      emailInput,
+      !emailInput.value.trim() ||
+        !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value.trim()),
+      "emailError"
+    ) && isValid;
+  isValid =
+    toggleValidation(
+      messageInput,
+      !messageInput.value.trim(),
+      "messageError"
+    ) && isValid;
 
   if (!isValid) return;
+
+  if (!recaptchaResponse) {
+    // todo Show an error message
+    console.error("Please complete the reCAPTCHA challenge.");
+    return;
+  }
 
   const formData = {
     name: nameInput.value.trim(),
     email: emailInput.value.trim(),
-    message: messageInput.value.trim()
+    message: messageInput.value.trim(),
+    recaptchaResponse: recaptchaResponse,
   };
 
   showLoadingSpinner();
 
-  fetch('/send-email', {
-    method: 'POST',
+  fetch("/send-email", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(formData),
   })
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       console.log(data);
       hideLoadingSpinner();
       showSuccessMessage();
     })
-    .catch(error => {
-      console.error('Error sending email:', error);
+    .catch((error) => {
+      console.error("Error sending email:", error);
       hideLoadingSpinner();
     });
 
@@ -62,20 +106,35 @@ function toggleValidation(inputField, condition, errorFieldId) {
 }
 
 function showLoadingSpinner() {
-  const loadingMessage = document.getElementById('loadingMessage');
-  loadingMessage.classList.remove('d-none');
+  const loadingMessage = document.getElementById("loadingMessage");
+  loadingMessage.classList.remove("d-none");
 }
 
-
 function hideLoadingSpinner() {
-  const loadingMessage = document.getElementById('loadingMessage');
-  loadingMessage.classList.add('d-none');
+  const loadingMessage = document.getElementById("loadingMessage");
+  loadingMessage.classList.add("d-none");
 }
 
 function showSuccessMessage() {
-  const successMessage = document.getElementById('successMessage');
-  successMessage.classList.remove('d-none');
+  const successMessage = document.getElementById("successMessage");
+  successMessage.classList.remove("d-none");
   setTimeout(() => {
-    successMessage.classList.add('d-none');
+    successMessage.classList.add("d-none");
   }, 2000);
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // todo Add reCAPTCHA
+  const recaptchaSiteKey = "site-key-here";
+  const recaptchaDiv = document.querySelector(".g-recaptcha");
+  recaptchaDiv.setAttribute("data-sitekey", recaptchaSiteKey);
+
+  const recaptchaScript = document.createElement("script");
+  recaptchaScript.src = "https://www.google.com/recaptcha/api.js";
+  recaptchaScript.async = true;
+  recaptchaScript.defer = true;
+  document.body.appendChild(recaptchaScript);
+
+  const submitButton = document.getElementById("submitButton");
+  submitButton.addEventListener("click", handleSubmit);
+});
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
+const axios = require('axios');
 const path = require('path');
+const app = express();
 const { sendEmail } = require('./public/js/sendEmail.js');
+
+//todo: fill in reCAPTCHA secret key
+const recaptchaSecretKey = 'FILL_IN';
+
+const crypto = require('crypto');
+const nonce = crypto.randomBytes(16).toString('base64');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,11 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   const userAgent = req.headers['user-agent'];
   if (userAgent.includes('Chrome')) {
-    res.setHeader('Content-Security-Policy', "script-src 'self' 'unsafe-inline' https://ajax.googleapis.com 'unsafe-inline' https://maxcdn.bootstrapcdn.com 'unsafe-inline' https://www.google-analytics.com");
+    res.setHeader('Content-Security-Policy', `script-src 'self' https://ajax.googleapis.com https://maxcdn.bootstrapcdn.com https://www.google-analytics.com https://www.google.com https://www.gstatic.com 'nonce-${nonce}'`);
   } else if (userAgent.includes('Edge')) {
     res.setHeader('Content-Security-Policy', "script-src 'self' 'unsafe-inline'");
   } else if (userAgent.includes('Firefox')) {
-    res.setHeader('Content-Security-Policy', "script-src 'self' 'unsafe-eval'");
+    res.setHeader('Content-Security-Policy', `script-src 'self' 'nonce-${nonce}'`);
   } else if (userAgent.includes('WebKit')) {
     res.setHeader('Content-Security-Policy', "script-src 'self'");
   } else {
@@ -25,17 +32,40 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/send-email', (req, res) => {
+app.post('/verify-recaptcha', async (req, res) => {
+  const { recaptchaResponse } = req.body;
+
+  try {
+    const recaptchaVerification = await axios.post(
+      'https://www.google.com/recaptcha/api/siteverify',
+      {
+        secret: recaptchaSecretKey,
+        response: recaptchaResponse,
+      }
+    );
+
+    if (recaptchaVerification.data.success) {
+      res.json({ success: true });
+    } else {
+      res.status(400).json({ success: false, error: 'reCAPTCHA verification failed' });
+    }
+  } catch (error) {
+    console.error('reCAPTCHA verification error:', error);
+    res.status(500).json({ success: false, error: 'Internal server error' });
+  }
+});
+
+app.post('/send-email', async (req, res) => {
   const { name, email, message } = req.body;
+
   sendEmail(name, email, message)
     .then(() => {
       res.json({ success: true });
     })
     .catch((error) => {
-      res.status(500).json({ success: false, error: 'Failed to send email' });
+      res.status(500).json({ success: false, error: error });
     });
 });
-
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
